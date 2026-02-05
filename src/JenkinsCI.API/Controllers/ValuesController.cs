dc5afb2129a6d44f1f7da30789571779cba6d4ab
bahriddin-af <bahriddinabdusalomov7@gmail.com>
2026-02-05 11:24:30 +0500

    test

--- src/JenkinsCI.API/Controllers/ValuesController.cs
+++ src/JenkinsCI.API/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ public class ValuesController : ControllerBase
     [HttpGet("{id}")]
     public string Get(int id)
     {
-        return "value";
+        return "succes"; 
     }
 
     // POST api/values
